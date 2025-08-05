5c85b9419b13f2a90133c596c84572e6299213a0
bernardohorn <pellizzarohornbernardo@gmail.com>
2025-08-05 13:27:10 -0300

    Ajustes finos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
-import Header from '../componentes/header.jsx';
-import Carrossel from '../componentes/Carousel.jsx';
-import Pg10 from '../pages/pg10.jsx';
+import Header from "../componentes/header.jsx";
+import Carrossel from "../componentes/Carousel.jsx";
+import Pg10 from "../pages/pg10.jsx";
 
 export default function HomePage() {
   return (
@@ -9,7 +9,7 @@ export default function HomePage() {
       <Carrossel />
       <Pg10 />
       {/* Conteúdo da página inicial pode ser adicionado aqui futuramente */}
-      <main style={{ minHeight: '80vh', background: '#f2f2f2' }}></main>
+      <main style={{ minHeight: "80vh", background: "#f2f2f2" }}></main>
     </>
   );
-} 
+}
